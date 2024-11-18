10fb27d6439ef0ebc7051b19310f96901ab35c9e
Damola <odamola@mit.edu>
2024-11-18 21:08:32 +0100

    added Title Components

---
+++ src/utils/interface.util.tsx
@@ -0,0 +1,12 @@
+import { ChangeEvent, RefObject } from "react";
+
+export interface ITitle {
+    text: string;
+    size?: string;
+    color?: string;
+    margin?: {
+      top?: string;
+      bottom?: string;
+    };
+  }
+  
