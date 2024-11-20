d03a712a130ebdf73687e164f0cbac553483e5fc
Damola <odamola@mit.edu>
2024-11-20 18:48:19 +0100

    added createTransaction Hook

---
+++ src/hooks/transaction/useCreateTransaction.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { createTransaction } from '../../api/transaction'; 
+import { ITransaction } from '../../utils/interface.util';
+
+export const useCreateTransaction = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [newTransaction, setNewTransaction] = useState<ITransaction | null>(null);
+
+  const createNewTransaction = async (transactionData: any) => {
+    setLoading(true);
+    try {
+      const createdTransaction = await createTransaction(transactionData);
+      setNewTransaction(createdTransaction);
+      
+    } catch (err) {
+      setError('Error creating transaction');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { createNewTransaction, newTransaction, loading, error };
+};
