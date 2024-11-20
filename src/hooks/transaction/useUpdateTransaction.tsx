5c69a5242fe9d23215d07660641a888b16e317da
Damola <odamola@mit.edu>
2024-11-20 18:41:25 +0100

    added updateTransaction Hook

---
+++ src/hooks/transaction/useUpdateTransaction.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { updateTransactionById } from '../../api/transaction'; 
+import { ITransaction } from '../../utils/interface.util'; 
+
+export const useUpdateTransaction = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [updatedTransaction, setUpdatedTransaction] = useState<ITransaction | null>(null);
+
+  const updateTransaction = async (id: string, updatedData: any) => {
+    setLoading(true);
+    try {
+      const updated = await updateTransactionById(id, updatedData);
+      setUpdatedTransaction(updated);
+      
+    } catch (err) {
+      setError('Error updating transaction');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { updateTransaction, updatedTransaction, loading, error };
+};
